ca5a20b5cfbafd51b906eeb1a04173989b506b26
Mark Vincze <mrk.vincze@gmail.com>
2020-12-02 17:24:13 +0100

    Switch from lists to sequences

--- FSharp/01-report.fsx
+++ FSharp/01-report.fsx
@@ -7,32 +7,36 @@ let numbers = File.ReadAllLines "01-report-input.txt"
 
 let rec pairs items =
     match items with
-    | [] | [_] -> []
-    | [ a; b ] -> [(a, b)]
-    | h :: t -> let ps1 = pairs t
-                let ps2 = t |> List.map (fun x -> (h, x))
-                List.concat [ ps1; ps2 ]
+    | [] | [_] -> Seq.ofList []
+    | [ a; b ] -> Seq.ofList [(a, b)]
+    | h :: t -> seq {
+                    for x in t do
+                        yield (h, x)
+                    yield! pairs t
+                }
 
 let a1, b1 =
     numbers
     |> pairs
-    |> List.find (fun (a,b) -> a + b = 2020)
+    |> Seq.find (fun (a,b) -> a + b = 2020)
 
 let result1 = a1 * b1
 
 let rec triplets items =
     match items with
-    | [] | [_] | [_;_] -> []
-    | [ a; b; c ] -> [(a, b, c)]
+    | [] | [_] | [_;_] -> Seq.ofList []
+    | [ a; b; c ] -> Seq.ofList [(a, b, c)]
     | h :: t -> let ts = triplets t
                 let ps = pairs t
-                [ for (a, b) in ps do 
-                    yield (a, b, h)
-                  yield! ts ]
+                seq {
+                    for (a, b) in ps do 
+                        yield (a, b, h)
+                    yield! ts
+                }
 
 let a2, b2, c2 =
     numbers
     |> triplets
-    |> List.find (fun (a, b, c) -> a + b + c = 2020)
+    |> Seq.find (fun (a, b, c) -> a + b + c = 2020)
 
 let result2 = a2 * b2 * c2
