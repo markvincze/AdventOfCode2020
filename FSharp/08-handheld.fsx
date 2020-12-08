34f314407683483867581065361561f50615bda6
Mark Vincze <mrk.vincze@gmail.com>
2020-12-08 22:38:32 +0100

    Day 8 part 2 F#

--- FSharp/08-handheld.fsx
+++ FSharp/08-handheld.fsx
@@ -7,14 +7,14 @@ type Outcome =
 | OutOfBound of int
 
 type Instruction =
-| Nop
+| Nop of int
 | Acc of int
 | Jmp of int
 
 let parse (line : string) =
     let s = line.Split(' ')
     match s.[0] with
-    | "nop" -> Nop
+    | "nop" -> Nop (s.[1] |> Int32.Parse)
     | "acc" -> Acc (s.[1] |> Int32.Parse)
     | "jmp" -> Jmp (s.[1] |> Int32.Parse)
     | _ -> failwith "Invalid input"
@@ -22,7 +22,7 @@ let parse (line : string) =
 let instructions = File.ReadAllLines "08-handheld-input.txt"
                    |> Array.map parse
 
-let rec runUntilRepeat (instructions : Instruction array) pc acc executed =
+let rec runProgram (instructions : Instruction array) pc acc executed =
     if pc > Array.length instructions
     then OutOfBound acc
     else if pc = Array.length instructions
@@ -30,11 +30,28 @@ let rec runUntilRepeat (instructions : Instruction array) pc acc executed =
     else if Set.contains pc executed
          then InfiniteLoop acc
          else match instructions.[pc] with
-              | Nop -> runUntilRepeat instructions (pc + 1) acc (Set.add pc executed)
-              | Acc arg -> runUntilRepeat instructions (pc + 1) (acc + arg) (Set.add pc executed)
-              | Jmp arg -> runUntilRepeat instructions (pc + arg) acc (Set.add pc executed)
+              | Nop _ -> runProgram instructions (pc + 1) acc (Set.add pc executed)
+              | Acc arg -> runProgram instructions (pc + 1) (acc + arg) (Set.add pc executed)
+              | Jmp arg -> runProgram instructions (pc + arg) acc (Set.add pc executed)
 
-let result1 = match runUntilRepeat instructions 0 0 Set.empty<int> with
+let result1 = match runProgram instructions 0 0 Set.empty<int> with
               | InfiniteLoop acc -> acc
               | _ -> failwith "Does not result in infinite loop"
 
+let findFixedResult instructions =
+    let rec findFixedResult (instructions : Instruction array) index =
+        if index >= Array.length instructions
+        then failwith "Didn't find a fix for the corruption"
+        else let inst = instructions.[index]
+             match inst with
+             | Jmp _ | Nop _ -> let replacement = match inst with | Jmp x -> Nop x | Nop x -> Jmp x
+                                instructions.[index] <- replacement
+                                match runProgram instructions 0 0 Set.empty<int> with
+                                | Terminated x -> x
+                                | _ -> instructions.[index] <- inst
+                                       findFixedResult instructions (index + 1)
+             | _ -> findFixedResult instructions (index + 1)
+
+    findFixedResult instructions 0
+
+let result2 = findFixedResult instructions
