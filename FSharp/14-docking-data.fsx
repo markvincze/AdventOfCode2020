42c80e848cf9ac54fec76b58ba7cf05189cfa0f2
Mark Vincze <mrk.vincze@gmail.com>
2020-12-15 23:29:50 +0100

    Day 14 part 2 F#

--- FSharp/14-docking-data.fsx
+++ FSharp/14-docking-data.fsx
@@ -17,7 +17,7 @@ let instructions = File.ReadAllLines "14-docking-data-input.txt"
                    |> Array.map parse
                    |> List.ofArray
 
-let processInstruction instruction memory (mask : string) =
+let processInstruction1 instruction memory (mask : string) =
     match instruction with
     | Mask m -> memory, m
     | Write (addr, value) ->
@@ -31,12 +31,55 @@ let processInstruction instruction memory (mask : string) =
 
         Map.add addr (Convert.ToInt64(sb.ToString(), 2)) memory, mask
 
-let rec processProgram instructions memory mask =
+let rec processProgram1 instructions memory mask =
     match instructions with
     | [] -> memory
-    | h :: t -> let memory, mask = processInstruction h memory mask
-                processProgram t memory mask
+    | h :: t -> let memory, mask = processInstruction1 h memory mask
+                processProgram1 t memory mask
 
-let finalMemory = processProgram instructions Map.empty<int64, int64> "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+let finalMemory1 = processProgram1 instructions Map.empty<int64, int64> "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-let result1 = finalMemory |> Seq.sumBy (fun kvp -> kvp.Value)
+let result1 = finalMemory1 |> Seq.sumBy (fun kvp -> kvp.Value)
+
+let rec allSubsets items =
+    match items with
+    | [] -> Seq.ofList [ [] ]
+    | h :: t -> let tSubsets = allSubsets t
+                seq {
+                    yield! allSubsets t
+                    for s in tSubsets do
+                        yield h :: s
+                }
+
+let allAddresses (address : int64) (mask : string) =
+    let valueBin = Convert.ToString(address, 2).PadLeft(36, '0') |> StringBuilder
+    mask |> Seq.iteri (fun i c -> if c = '1' then valueBin.[i] <- '1')
+
+    let xIndexes = mask |> Seq.indexed |> Seq.filter (fun (i, c) -> c = 'X') |> Seq.map fst |> List.ofSeq
+
+    seq {
+        for ones in allSubsets xIndexes do
+            ones |> List.iter (fun i -> valueBin.[i] <- '1')
+            xIndexes |> List.except ones |> List.iter (fun i -> valueBin.[i] <- '0')
+            yield Convert.ToInt64(valueBin.ToString(), 2)
+    }
+
+let processInstruction2 instruction memory (mask : string) =
+    match instruction with
+    | Mask m -> memory, m
+    | Write (addr, value) ->
+        let addresses = allAddresses addr mask
+        let newMemory =
+            addresses
+            |> Seq.fold (fun mem addr -> Map.add addr value mem) memory
+        newMemory, mask
+
+let rec processProgram2 instructions memory mask =
+    match instructions with
+    | [] -> memory
+    | h :: t -> let memory, mask = processInstruction2 h memory mask
+                processProgram2 t memory mask
+
+let finalMemory2 = processProgram2 instructions Map.empty<int64, int64> "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+let result2 = finalMemory2 |> Seq.sumBy (fun kvp -> kvp.Value)
