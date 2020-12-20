151dae77f60de49b074f7929bf706a21f374eaa8
Mark Vincze <mrk.vincze@gmail.com>
2020-12-20 23:37:00 +0100

    Day 19 F# part 1

---
+++ FSharp/19-monster-messages.fsx
@@ -0,0 +1,75 @@
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+type Rule =
+| Char of char
+| Sequence of Rule list
+| Choice of Rule * Rule
+
+let input = File.ReadAllLines "19-monster-messages-input.txt"
+
+let doesMatch rule str =
+    let rec doesMatch rule (str : string) index =
+        match rule with
+        | Char char -> match str.[index] with
+                       | c when c = char -> true, (index + 1)
+                       | _ -> false, index
+        | Choice (r1, r2) -> let m1, i1 = doesMatch r1 str index
+                             let m2, i2 = doesMatch r2 str index
+                             if m1 then m1, i1
+                             else if m2 then m2, i2
+                             else false, index
+        | Sequence rules -> List.fold
+                                (fun (m, i) r -> if m then doesMatch r str i
+                                                 else m, i)
+                                (true, index)
+                                rules
+    let (m, i) = doesMatch rule str 0
+    m && i = (str.Length)
+
+let ids (line : string) = let ruleStr = (line.Split ": ").[1]
+                          printfn "Rule: %s" ruleStr
+                          if ruleStr.StartsWith "\"" 
+                          then [||]
+                          else ruleStr.Split([| ' '; '|' |], StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+                               |> Array.map Int32.Parse
+
+let parseSeq ruleCache (str : string) =
+    str.Split ' '
+    |> Array.map (fun rid -> Map.find (Int32.Parse rid) ruleCache)
+    |> List.ofArray
+    |> Sequence
+
+let rec parse (rulesInput : string array) ruleCache =
+    let nextLine =
+        rulesInput
+        |> Array.tryFind (fun line -> let ruleId = (line.Split ':').[0] |> Int32.Parse
+                                      not (Map.containsKey ruleId ruleCache) &&
+                                      ids line |> Array.forall (fun i -> Map.containsKey i ruleCache))
+    match nextLine with
+    | None -> ruleCache.[0]
+    | Some line ->
+        let segments = line.Split ": "
+        let ruleId = segments.[0] |> Int32.Parse
+        let ruleStr = segments.[1]
+        let rule = if ruleStr.StartsWith "\""
+                   then Char (ruleStr.Trim '"').[0]
+                   else if ruleStr.Contains "|"
+                   then let [|seq1; seq2|] = ruleStr.Split " | "
+                        Choice (parseSeq ruleCache seq1, parseSeq ruleCache seq2)
+                   else parseSeq ruleCache ruleStr
+        parse rulesInput (Map.add ruleId rule ruleCache)
+
+let rulesInput = input
+                 |> Array.takeWhile (fun l -> l <> "")
+
+let rootRule = parse rulesInput Map.empty<int, Rule>
+
+let testInput = input
+                |> Array.skipWhile (fun l -> l <> "")
+                |> Array.skip 1
+
+let result1 = testInput
+              |> Seq.filter (doesMatch rootRule)
+              |> Seq.length
